0b936f4af8f015f20b39d869f2fb99555c20f249
fengjiao <fengjiao.ji@uzh.ch>
2023-04-12 18:09:22 +0200

    fix null roomCode when join subscribing

--- src/components/views/RoomEntrance.js
+++ src/components/views/RoomEntrance.js
@@ -55,11 +55,7 @@ const RoomEntrance = props => {
                 if (!client.isconnected) {
                     client.connect({}, function (frame) {
 						console.log('connected to stomp');
-						client.subscribe('/topic/multi/rooms' + roomcode + '/join', function (response){
-							const room = response.body;
-							const roomparse = JSON.parse(room);
-							history.push("/rooms/" + roomparse["roomID"] + "/participants");
-						});
+						
                     });
                 }
             } catch (error) {
@@ -80,8 +76,16 @@ const RoomEntrance = props => {
     }, []);
 
 	const enterRoom = () => {
-		const requestBody = JSON.stringify({userID});
-		client.send('/app/multi/rooms/' + roomcode + '/join', {}, requestBody);
+		const requestBody = JSON.stringify({ userID });
+		client.subscribe('/topic/multi/rooms/' + roomcode + '/join', function (response) {
+			console.log(response.body)
+			const room = response.body;
+			const roomparse = JSON.parse(room);
+			history.push("/rooms/" + roomparse["roomID"] + "/participants");
+		});
+		setTimeout(function () {
+			client.send('/app/multi/rooms/' + roomcode + '/join', {}, requestBody);
+		},100);
 	};
 
 	return (
