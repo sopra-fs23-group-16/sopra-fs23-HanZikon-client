5b3a7136bf3cb7f2ff2af8468cb35fec1b4f1389
fengjiao <fengjiao.ji@uzh.ch>
2023-04-23 17:22:12 +0200

    go to different page according to questionType

--- src/helpers/nextRound.js
+++ src/helpers/nextRound.js
@@ -1,15 +1,14 @@
 export const nextRound = (roomID) => {
-    // round++
     let round = parseInt(localStorage.getItem("round"));
-    // check if round == 10 (end game)
+    /**check if round == 10 (end game)*/
     if (round == 10) {
         // go back to lobby for now
         // waitingRoom need clarify owner or player
         window.location.href = "/lobby"
     }
+    // round++
     round = round + 1;
     localStorage.setItem("round", round);
-
     // clear roundPoints
     localStorage.removeItem("roundPoints");
     // check questionType
@@ -17,5 +16,7 @@ export const nextRound = (roomID) => {
     const nextQuestion = questionList[round - 1];
     if (nextQuestion.questionType == "MultipleChoice") {
         window.location.href = "/games/multiplechoice/" + roomID;
+    } else {
+        window.location.href = '/games/imitation/' + roomID;
     }
 }
