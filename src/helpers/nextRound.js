9791a2d85a1aa3860812aad79b5aa9a4cf672b89
fengjiao <fengjiao.ji@uzh.ch>
2023-05-07 21:07:39 +0200

    fix final round error

--- src/helpers/nextRound.js
+++ src/helpers/nextRound.js
@@ -10,18 +10,19 @@ export const nextRound = (roomID) => {
         // go back to lobby for now
         // waitingRoom need clarify owner or player
         window.location.href = "/lobby"
-    }
-    // round++
-    round = round + 1;
-    localStorage.setItem("round", round);
-    // clear roundPoints
-    localStorage.removeItem("roundPoints");
-    // check questionType
-    const questionList = JSON.parse(localStorage.getItem('questionList'));
-    const nextQuestion = questionList[round - 1];
-    if (nextQuestion.questionType == "MultipleChoice") {
-        window.location.href = "/games/multiplechoice/" + roomID;
-    } else {
-        window.location.href = '/games/imitationinspect/' + roomID;
+    }else{
+        // round++
+        round = round + 1;
+        localStorage.setItem("round", round);
+        // clear roundPoints
+        localStorage.removeItem("roundPoints");
+        // check questionType
+        const questionList = JSON.parse(localStorage.getItem('questionList'));
+        const nextQuestion = questionList[round - 1];
+        if (nextQuestion.questionType == "MultipleChoice") {
+            window.location.href = "/games/multiplechoice/" + roomID;
+        } else {
+            window.location.href = '/games/imitationinspect/' + roomID;
+        }
     }
 }
