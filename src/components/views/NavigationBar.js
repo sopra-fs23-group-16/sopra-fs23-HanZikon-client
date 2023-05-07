6faba50832523d6debda9a514fcb922401aff709
Linsey <mshk8638@gmail.com>
2023-05-07 21:14:05 +0200

    change way to redirect for navigation bar

--- src/components/views/NavigationBar.js
+++ src/components/views/NavigationBar.js
@@ -3,29 +3,10 @@ import "styles/views/NavigationBar.scss";
 import { useHistory } from "react-router-dom";
 import Logo from "image/logo.jpg";
 import {api} from 'helpers/api';
-import User from 'models/User';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 
 const NavigationBar = () => {
-    useEffect(() => {
-		// fetch localuser's information by token
-		async function fetchLocalUser() {
-			try {
-				const requestBody = JSON.stringify({ token: localStorage.getItem("token") });
-				const response = await api.post(`/users/localUser`, requestBody);
 
-				const user = new User(response.data);
-				console.log("Confirm local user:",user);
-				localStorage.setItem('loggedInUser', user.id);
-
-			} catch (error) {
-				alert("You are not logged in!");
-				localStorage.removeItem('token');
-				history.push('/login');
-			}
-		}
-		fetchLocalUser();
-	}, []);
 
 	const history = useHistory();
 	const [users, setUsers] = useState(null);
@@ -35,9 +16,9 @@ const NavigationBar = () => {
 		localStorage.removeItem('token');
 		localStorage.removeItem("loggedInUser");
 		const response = api.get('/logout/'+ id);
-		history.push('/login');
+		window.location.href = '/login';
 	}
-
+    
 
   return (
     <div className="navbar container">
@@ -46,8 +27,8 @@ const NavigationBar = () => {
       </div>
 
       <div className="navbar button-container">
-            <div className="navbar button" onClick={() => history.push(`/lobby`)}><i class="material-icons md-36 text-red">home</i></div>
-            <div className="navbar button" onClick={() => history.push(`/users/${id}`)}><i class="material-icons md-36 text-red">person</i></div>
+            <div className="navbar button" onClick={() => window.location.href = "/lobby"}><i class="material-icons md-36 text-red">home</i></div>
+            <div className="navbar button" onClick={() => window.location.href = "/users/" + id}><i class="material-icons md-36 text-red">person</i></div>
             <div className="navbar button" onClick={() => logout()}><i class="material-icons md-36 text-red">logout</i></div>
       </div>
     </div>
