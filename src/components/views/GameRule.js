02e01fc4fab49a115962e0972964a8870e55b6aa
Linsey <mshk8638@gmail.com>
2023-05-08 10:44:54 +0200

    change game rule color

--- src/components/views/GameRule.js
+++ src/components/views/GameRule.js
@@ -52,12 +52,12 @@ const GameRule = () => {
 					a. Learn to write Chinese characters from Live Demo and a guided imitation.<br />
 					b. Browse the evolution of Chinese characters dating back to 2nd millennium BC.<br />
 					c. "Draw" the Chinese character from your memory.<br />
-					d.Peer review all your teammates’ works and take a vote!.<br /><br />
+					d. Peer review all your teammates’ works and take a vote!<br /><br />
 					<b>2. OracleGuessing</b><br /><br />
 					a. An Oracle bone script, together with the English meaning of the word, will be displayed.<br />
 					b. Four choices of modern Chinese characters will be displayed.<br />
 					c. Only one answer is correct.<br /><br />
-					<b>Note:</b> Empirical evidence suggests, a prior knowledge in Chinese isn’t going to help. Just trust your gut and have fun!<br /><br />
+					<b>Note: </b> Empirical evidence suggests, a prior knowledge in Chinese isn’t going to help. Just trust your gut and have fun!<br /><br />
 					<b>3. A bit of Both</b><br /><br />
 					a. Just like its name, a bit of both modes coming in random order.
                     </div>
