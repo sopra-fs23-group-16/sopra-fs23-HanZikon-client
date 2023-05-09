52c70533a9cb43ce983bdffa31506b05d3a52bb7
Fitz <fitzlihm00@gmail.com>
2023-05-09 09:39:38 +0200

    Add Translation API (Trial 2)

--- src/helpers/getTranslation.js
+++ src/helpers/getTranslation.js
@@ -1,33 +1,17 @@
-
-const getTranslation = (cn_character) => {
-    let fromLang = 'cn'; // from Chinese
+const getTranslationURL = (cn_character) => {
+    let fromLang = 'zh'; // from Chinese
     let toLang = 'en'; // translate to English
     let text = cn_character;
-    console.log("character to be tranlate is"+ cn_character)
+    console.log("character to be translated is " + cn_character);
 
-    const API_KEY = ["AIzaSyDAh5E59SyjmGb1MVMcruOnwlBmr-D9F-o"];
+    const API_KEY = "AIzaSyDAh5E59SyjmGb1MVMcruOnwlBmr-D9F-o";
 
     let url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
     url += '&q=' + encodeURI(text);
     url += `&source=${fromLang}`;
     url += `&target=${toLang}`;
 
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        }
-    })
-        .then(res => res.json())
-        .then((response) => {
-            console.log("response from google: ", response);
-            return response;
-        })
-        .catch(error => {
-            console.log("There was an error with the translation request: ", error);
-        });
-
+    return url;
 }
 
-export default getTranslation;
+export default getTranslationURL;
