e5b4426964c0b7a6d73d0846fdebc4ae64ab2b23
Songlin Wang <Songlin.Wang@uzh.ch>
2023-05-10 22:29:38 +0200

    imitation voting page rendering

--- src/components/views/ImitationVote.js
+++ src/components/views/ImitationVote.js
@@ -5,7 +5,6 @@ import BaseContainer from "components/ui/BaseContainer";
 import dog from 'image/dog.png';
 import recognizeHandwriting from "../../helpers/recognizeHandwriting";
 import 'styles/views/ImitationVote.scss';
-import 'styles/views/ChoiceGame.scss';
 import User from 'models/User';
 import {fetchLocalUser} from "../../helpers/confirmLocalUser";
 
@@ -31,7 +30,6 @@ const ImitationVote = props => {
 						console.log('connected to stomp');
 
 						client.subscribe('/topic/multi/rooms/' + roomID + '/imitations', function (response) {
-							setPlayers([]);
 							const playersImitations = response.body;
 							const playersImitationsParse = JSON.parse(playersImitations);
 							const playersImitationsArray = Object.entries(playersImitationsParse)
@@ -89,7 +87,7 @@ const ImitationVote = props => {
 
 	window.addEventListener("load", function() {
 
-		var countdown = 20;
+		var countdown = 600;
 		var countdownElement = document.getElementById("countdown");
 
 		var timer = setInterval(function() {
@@ -108,67 +106,67 @@ const ImitationVote = props => {
 
 	return (
 		<BaseContainer>
-			<div className="choicegame container">
-				<div className="choicegame col">
+			<div className="imitationvote container">
+				<div className="imitationvote col">
 
 					{players.length > 0 ? (
-						<div className="choicegame card">
+						<div className="imitationvote card">
 							<img src={dog} alt="player1" style={{ width: '80%', height: 'auto', display: 'block', margin: 'auto' }} />
 						</div>) : null}
 					{playerNames.length > 0 ? (
-						<div className="choicegame label"> {playerNames[0]}</div>
+						<div className="imitationvote label"> {playerNames[0]}</div>
 					) : null}
 					{players.length > 1 ? (
-						<div className="choicegame card">
+						<div className="imitationvote card">
 							<img src={dog} alt="player1" style={{ width: '80%', height: 'auto', display: 'block', margin: 'auto' }} />
 						</div>) : null}
 					{playerNames.length > 1 ? (
-						<div className="choicegame label"> {playerNames[1]}</div>
+						<div className="imitationvote label"> {playerNames[1]}</div>
 					) : null}
 
 					{players.length > 2 ? (
-						<div className="choicegame card">
+						<div className="imitationvote card">
 							<img src={dog} alt="player1" style={{ width: '80%', height: 'auto', display: 'block', margin: 'auto' }} />
 						</div>) : null}
 					{playerNames.length > 2 ? (
-						<div className="choicegame label"> {playerNames[2]}</div>
+						<div className="imitationvote label"> {playerNames[2]}</div>
 					) : null}
 
 					{players.length > 3 ? (
-						<div className="choicegame card">
+						<div className="imitationvote card">
 							<img src={dog} alt="player1" style={{ width: '80%', height: 'auto', display: 'block', margin: 'auto' }} />
 						</div>) : null}
 					{playerNames.length > 3 ? (
-						<div className="choicegame label"> {playerNames[3]}</div>
+						<div className="imitationvote label"> {playerNames[3]}</div>
 					) : null}
 
 					{players.length > 4 ? (
-						<div className="choicegame card">
+						<div className="imitationvote card">
 							<img src={dog} alt="player1" style={{ width: '80%', height: 'auto', display: 'block', margin: 'auto' }} />
 						</div>) : null}
 					{playerNames.length > 4 ? (
-						<div className="choicegame label"> {playerNames[4]}</div>
+						<div className="imitationvote label"> {playerNames[4]}</div>
 					) : null}
 
 					{players.length > 5 ? (
-						<div className="choicegame card">
+						<div className="imitationvote card">
 							<img src={dog} alt="player1" style={{ width: '80%', height: 'auto', display: 'block', margin: 'auto' }} />
 						</div>) : null}
 					{playerNames.length > 5 ? (
-						<div className="choicegame label"> {playerNames[5]}</div>
+						<div className="imitationvote label"> {playerNames[5]}</div>
 					) : null}
 
 					{players.length > 6 ? (
-						<div className="choicegame card">
+						<div className="imitationvote card">
 							<img src={dog} alt="player1" style={{ width: '80%', height: 'auto', display: 'block', margin: 'auto' }} />
 						</div>) : null}
 					{playerNames.length > 6 ? (
-						<div className="choicegame label"> {playerNames[6]}</div>
+						<div className="imitationvote label"> {playerNames[6]}</div>
 					) : null}
 
 				</div>
 
-				<div className="choicegame form">
+				<div className="imitationvote form">
 					<center>
 						<div id="countdown" className="">
 						</div>
@@ -181,8 +179,8 @@ const ImitationVote = props => {
 							<img id="playerImitation5" src="" style={{ width: '15%', height: 'auto', margin: 'auto' }}/>
 						</div>
 						
-						<div className="choicegame label"> {"Like it"}</div>
-						<div className="choicegame label"> {"Like it"}</div>
+						<div className="imitationvote label"> {"Like it"}</div>
+						<div className="imitationvote label"> {"Like it"}</div>
 					</center>
 				</div>
 			</div>
