a312d8688763ed3bd266c4fe2abb0e47557873a4
Fitz <fitzlihm00@gmail.com>
2023-05-18 12:10:19 +0200

    Change Text Mapping

--- src/helpers/normalizeGameMode.js
+++ src/helpers/normalizeGameMode.js
@@ -1,11 +1,11 @@
 export const normalizeGameMode = (gameMode) =>{
     switch(gameMode) {
         case "MultipleChoice":
-            return("Oracle Guessing Multiple Choice")
+            return("Oracle Script Guessing")
         case "Mixed":
             return("Bit of Both")
         case "HanziDrawing":
-            return("Hanzi Imitation Drawing")
+            return("Hanzi Imitation")
         default:
             return(gameMode)
     }
