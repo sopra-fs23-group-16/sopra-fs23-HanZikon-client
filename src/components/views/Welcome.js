a1068f329606fd3a0f3c2aa99990e89cfd593417
Linsey <mshk8638@gmail.com>
2023-05-20 16:17:16 +0200

    add motivation text in welcome page

--- src/components/views/Welcome.js
+++ src/components/views/Welcome.js
@@ -7,9 +7,27 @@ const Welcome = () => {
 
 	return (
 		<BaseContainer>
+		<br />
+		<br />
+		<br />
 			<div className="welcome container">
-				<div className="welcome label">Welcome to Hanzikon!</div>
-				<img src={Logo} style={{ width: '50%', height: 'auto', display: 'block', marginLeft: "8%" }}/>
+				{/* <div className="welcome label">Welcome to </div> */}
+				<img src={Logo} style={{ width: '60%', height: 'auto', display: 'block', marginLeft: "15%" }}/>
+				<div className="welcome label-motivation">
+					<center>
+			We are living in a world that is more connected than ever before, and it has become inevitable to
+			encounter things from other cultures, amongst which Chinese is a prominent part. <br /><br />
+			As the second most spoken language in the world, Chinese characters have a distinctive form from Latin letters.
+			That may explain that for people from other cultural backgrounds, they merely look like a sketch
+			of random lines and dots. <br /><br />
+			As a group of five native Chinese speakers, we know how tedious and hard it could be to learn to write Chinese characters, 
+			and that's what inspires us to create this highly recreational, intuitive while educational web app, to enable those curious minds to explore
+			the structural beauty of Chinese characters and learn a little bit about the logographic properties
+			of them. <br /><br />
+			We firmly believe that learning alone is boring and discouraging, so our primary mode
+			consists of several players competing together, thus utilizing the potential of a web application.
+			</center>
+			</div>
 					<div className="welcome form">
 						<div className="welcome button-container">
 							<PrimaryButton
