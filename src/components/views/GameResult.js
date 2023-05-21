2ff46c987f3261df7a35df6fe0bb53e4fdb5e5b7
Fitz <fitzlihm00@gmail.com>
2023-05-21 12:02:35 +0200

    Update Firework Effect at last round

--- src/components/views/GameResult.js
+++ src/components/views/GameResult.js
@@ -10,6 +10,9 @@ import {Spinner} from "../ui/Spinner";
 import {SecondaryButton} from "../ui/SecondaryButton";
 import { FaHeart } from "react-icons/fa";
 import AnimationItems from "helpers/FlowerVisualEffect"
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
 
 const GameResult = props => {
 	
@@ -20,8 +23,25 @@ const GameResult = props => {
 	const [votedTimes, setVotedTimes] = useState([]);
 	const [showButton, setShowButton] = useState(false);
 	//const playerNames = players.length > 0 ? players.map(player => player.playerName) : [];
-	const animation = new AnimationItems();
+	//const animation = new AnimationItems();
 	const currentQuesType = (questionList[round-1]).questionType;
+	const [isParticle, setShowParticles] = useState(true);
+
+	// const [particlesInit, setParticleInit] = useState();
+	// const [particlesLoaded, setParticlesLoaded] = useState();
+
+	const particlesInit = useCallback(async engine => {
+		console.log(engine);
+		// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(engine);
+	}, []);
+
+	const particlesLoaded = useCallback(async container => {
+		await console.log(container);
+	}, []);
+
 
 	useEffect(() => {
 		// startCountdown();
@@ -85,13 +105,38 @@ const GameResult = props => {
 	}, []);
 
 	useEffect(() => {
-		if (localStorage.getItem("round") === localStorage.getItem("numRound")) {
-			animation.start();//开始
-			setTimeout(() => {
-				animation.stop();//停止
-			}, 10000)
-		}
+		const timeout = setTimeout(() => {
+			setShowParticles(false);
+		}, 15000); // Set the timeout duration in milliseconds (e.g., 5000ms = 5 seconds)
+
+		return () => clearTimeout(timeout); // Clean up the timeout on component unmount
 	}, []);
+
+
+	// useEffect(() => {
+	// 	// if (localStorage.getItem("round") === localStorage.getItem("numRound")) {
+	// 	// 	animation.start();//开始
+	// 	// 	setTimeout(() => {
+	// 	// 		animation.stop();//停止
+	// 	// 	}, 10000)
+	// 	// }
+	//
+	// 	if (localStorage.getItem("round") === localStorage.getItem("numRound")) {
+	// 		const particlesInit = useCallback(async engine => {
+	// 			console.log(engine);
+	// 			// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+	// 			// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+	// 			// starting from v2 you can add only the features you need reducing the bundle size
+	// 			await loadFull(engine);
+	// 		}, []);
+	// 		setParticleInit(particlesInit)
+	//
+	// 		const particlesLoaded = useCallback(async container => {
+	// 			await console.log(container);
+	// 		}, []);
+	// 		setParticlesLoaded(particlesLoaded)
+	// 	}
+	// }, []);
 	
 	// const startCountdown = () => {
 	//
@@ -179,8 +224,199 @@ const GameResult = props => {
 								</SecondaryButton>}
 						</div>
 					)}
+
 				</center>
 			</div>
+				{ localStorage.getItem("round") === localStorage.getItem("numRound") &&
+					isParticle &&
+					(<Particles
+						className="gameresult particles"
+						id="tsparticles"
+						init={particlesInit}
+						loaded={particlesLoaded}
+						options={{
+							"name": "Fireworks",
+							"fullScreen": {
+								"enable": true
+							},
+							"background": {
+								"color": "#ecf4f4"
+							},
+							"emitters": {
+								"direction": "top",
+								"life": {
+									"count": 0,
+									"duration": 0.1,
+									"delay": 0.1
+								},
+								"rate": {
+									"delay": 0.15,
+									"quantity": 1
+								},
+								"size": {
+									"width": 100,
+									"height": 0
+								},
+								"position": {
+									"y": 100,
+									"x": 50
+								}
+							},
+							"particles": {
+								"number": {
+									"value": 0
+								},
+								"destroy": {
+									"bounds": {
+										"top": 30
+									},
+									"mode": "split",
+									"split": {
+										"count": 1,
+										"factor": {
+											"value": 0.333333
+										},
+										"rate": {
+											"value": 80
+										},
+										"particles": {
+											"stroke": {
+												"width": 0
+											},
+											"color": {
+												"value": ["#ff595e", "#ffca3a", "#8ac926", "#1982c4", "#6a4c93"]
+											},
+											"number": {
+												"value": 0
+											},
+											"collisions": {
+												"enable": false
+											},
+											"destroy": {
+												"bounds": {
+													"top": 0
+												}
+											},
+											"opacity": {
+												"value": {
+													"min": 0.1,
+													"max": 1
+												},
+												"animation": {
+													"enable": true,
+													"speed": 0.7,
+													"sync": false,
+													"startValue": "max",
+													"destroy": "min"
+												}
+											},
+											"shape": {
+												"type": "circle"
+											},
+											"size": {
+												"value": 2,
+												"animation": {
+													"enable": false
+												}
+											},
+											"life": {
+												"count": 1,
+												"duration": {
+													"value": {
+														"min": 1,
+														"max": 2
+													}
+												}
+											},
+											"move": {
+												"enable": true,
+												"gravity": {
+													"enable": true,
+													"acceleration": 9.81,
+													"inverse": false
+												},
+												"decay": 0.1,
+												"speed": {
+													"min": 10,
+													"max": 25
+												},
+												"direction": "outside",
+												"random": true,
+												"straight": false,
+												"outModes": "destroy"
+											}
+										}
+									}
+								},
+								"life": {
+									"count": 1
+								},
+								"shape": {
+									"type": "line"
+								},
+								"size": {
+									"value": {
+										"min": 0.1,
+										"max": 50
+									},
+									"animation": {
+										"enable": true,
+										"sync": true,
+										"speed": 90,
+										"startValue": "max",
+										"destroy": "min"
+									}
+								},
+								"stroke": {
+									"color": {
+										"value": "rgb(255, 255, 255,0)"
+									},
+									"width": 1
+								},
+								"rotate": {
+									"path": true
+								},
+								"move": {
+									"enable": true,
+									"gravity": {
+										"acceleration": 15,
+										"enable": true,
+										"inverse": true,
+										"maxSpeed": 100
+									},
+									"speed": {
+										"min": 10,
+										"max": 20
+									},
+									"outModes": {
+										"default": "destroy",
+										"top": "none"
+									},
+									"trail": {
+										"fillColor": "#ecf4f4",
+										"enable": true,
+										"length": 10
+									}
+								}
+							},
+							"sounds": {
+								"enable": false,
+								"events": [
+									{
+										"event": "particleRemoved",
+										"filter": "explodeSoundCheck",
+										"audio": [
+											"https://particles.js.org/audio/explosion0.mp3",
+											"https://particles.js.org/audio/explosion1.mp3",
+											"https://particles.js.org/audio/explosion2.mp3"
+										]
+									}
+								],
+								"volume": 50
+							}
+						}}
+					/>)
+				}
 			</div>
 		)
 	}
