f47436880f1a42df5c8507c5d860a854f438c632
Fitz <fitzlihm00@gmail.com>
2023-05-22 11:59:43 +0200

    Update text and Style at Welcome Page

--- src/components/views/Welcome.js
+++ src/components/views/Welcome.js
@@ -14,20 +14,18 @@ const Welcome = () => {
 				{/* <div className="welcome label">Welcome to </div> */}
 				<img src={Logo} style={{ width: '60%', height: 'auto', display: 'block', marginLeft: "15%" }}/>
 				<div className="welcome label-motivation">
-					<center>
-			We are living in a world that is more connected than ever before, and it has become inevitable to
-			encounter things from other cultures, amongst which Chinese is a prominent part. <br /><br />
+					<br />
 			As the second most spoken language in the world, Chinese characters have a distinctive form from Latin letters.
-			That may explain that for people from other cultural backgrounds, they merely look like a sketch
-			of random lines and dots. <br /><br />
-			As a group of five native Chinese speakers, we know how tedious and hard it could be to learn to write Chinese characters, 
-			and that's what inspires us to create this highly recreational, intuitive while educational web app, to enable those curious minds to explore
-			the structural beauty of Chinese characters and learn a little bit about the logographic properties
-			of them. <br /><br />
-			We firmly believe that learning alone is boring and discouraging, so our primary mode
-			consists of several players competing together, thus utilizing the potential of a web application.
-			</center>
-			</div>
+			For people from other cultural backgrounds, they merely look like a sketch of random lines and dots.
+					Despite the complexity, they actually contain meanings in the form.<br /><br />
+
+			As a group of five native Chinese speakers, we know how tedious and hard it could be to learn to write Chinese characters,
+			That's what inspired us to create this highly recreational and intuitive game, enabliing those curious minds to explore
+			the structural beauty of Chinese characters and have a glimpse into Chinese culture. <br /><br />
+
+			Learning alone could be boring and discouraging. Invite your friends and have a try of the 3 Games Modes we offer.	<br />
+
+				</div>
 					<div className="welcome form">
 						<div className="welcome button-container">
 							<PrimaryButton
