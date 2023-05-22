f68a751be0dd466688f949c9cf318493c1149e6c
Linsey <mshk8638@gmail.com>
2023-05-22 20:10:14 +0200

    optimize welcome page

--- src/components/views/Welcome.js
+++ src/components/views/Welcome.js
@@ -7,12 +7,9 @@ const Welcome = () => {
 
 	return (
 		<BaseContainer>
-		<br />
-		<br />
-		<br />
+
 			<div className="welcome container">
-				{/* <div className="welcome label">Welcome to </div> */}
-				<img src={Logo} style={{ width: '60%', height: 'auto', display: 'block', marginLeft: "15%" }}/>
+				<img src={Logo} style={{ width: '50%', height: 'auto', display: 'block', marginLeft: "15%" }}/>
 				<div className="welcome label-motivation">
 					<br />
 			As the second most spoken language in the world, Chinese characters have a distinctive form from Latin letters.
@@ -20,11 +17,11 @@ const Welcome = () => {
 					Despite the complexity, they actually contain meanings in the form.<br /><br />
 
 			As a group of five native Chinese speakers, we know how tedious and hard it could be to learn to write Chinese characters,
-			That's what inspired us to create this highly recreational and intuitive game, enabliing those curious minds to explore
+			that's what inspired us to create this highly recreational and intuitive game, enabliing those curious minds to explore
 			the structural beauty of Chinese characters and have a glimpse into Chinese culture. <br /><br />
 
-			Learning alone could be boring and discouraging. Invite your friends and have a try of the 3 Games Modes we offer.	<br />
-
+			Learning alone could be boring and discouraging. Invite your friends and have a try of the 3 games ,odes we offer.	
+			<br /><br />
 				</div>
 					<div className="welcome form">
 						<div className="welcome button-container">
